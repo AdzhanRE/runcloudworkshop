79e61d9c2dff521e1287cc01215cfe8f8e200b61
AdzhanRE <56430962+AdzhanRE@users.noreply.github.com>
2019-10-11 17:06:37 +0800

    Add files via upload

---
+++ test.php
@@ -0,0 +1,17 @@
+<?php
+    $favcolor = "p";
+
+    switch($favcolor){
+        case "red":
+            echo "Your Favourite color is red";
+            break;
+        case "green":
+            echo "Your Favourite color is green";
+            break;
+        case "blue":
+            echo "Your Favourite color is blue";
+            break;
+        default:
+            echo "You don't have favourite color";
+    }
+?>
